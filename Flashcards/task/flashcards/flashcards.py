df6644ef391808535ba757614caa6c9581dbeee0
Antonio Marí <amari@at4.net>
2021-10-02 19:23:14 +0200

    Stage 3/7

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,8 +1,18 @@
-term = input()
-definition = input()
-answer = input()
+print("Input the number of cards:")
+number_of_cards = int(input())
 
-if definition == answer:
-    print("Your answer is right!")
-else:
-    print("Your answer is wrong...")
+flashcards = []
+for card_number in range(1, number_of_cards + 1):
+    print(f"The term for card #{card_number}:")
+    term = input()
+    print(f"The definition for card #{card_number}:")
+    definition = input()
+    flashcards.append([term, definition])
+
+for flashcard in flashcards:
+    print(f'Print the definition of "{flashcard[0]}":')
+    answer = input()
+    if answer == flashcard[1]:
+        print("Correct!")
+    else:
+        print(f'Wrong. The right answer is "{flashcard[1]}".')
